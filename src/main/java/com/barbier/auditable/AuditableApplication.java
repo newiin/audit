90ebcd486b52924f5a19896cfbe6473d2270f587
carlos barbier <carlos.barbier@outlook.com>
2020-03-09 21:11:20 +0000

    first commit

---
+++ src/main/java/com/barbier/auditable/AuditableApplication.java
@@ -0,0 +1,13 @@
+package com.barbier.auditable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuditableApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuditableApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/barbier/auditable/AuditableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.barbier.auditable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuditableApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
